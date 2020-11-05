b964d9cb6cfcb4a8cb1e8fed33f0af179a81592f
KENCHku <kench.mkk3@gmail.com>
2020-11-05 17:31:26 +0600

    Initial commit

---
+++ src/GenericInterface.java
@@ -0,0 +1,15 @@
+public interface GenericInterface <T>{
+
+
+    T getDigit();
+    T getText();
+
+    void setDigit (T digit);
+    void setText (T text);
+
+
+
+   /* T getDigit();
+
+    void setDigit(T digit);//в интерфейсе методы не реализуются и тела не нужны*/
+    }
